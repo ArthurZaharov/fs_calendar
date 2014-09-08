8bd7cdbe9c39ceeac568c5492280ef7efb5f0d5b
Artur Zaharov <artur.zaharov@gmail.com>
2014-09-08 23:46:18 +0400

    Sign in users

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature "Users sign in", :type => :feature do
+	let(:user) { create(:user) }
+
+	scenario "fail with blank email" do
+		sign_in_with '', user.password
+
+		expect(page).to have_text('Invalid email or password')
+	end
+
+	scenario "fail with wrong email" do
+		sign_in_with 'blabla@example.com', user.password
+
+		expect(page).to have_text('Invalid email or password')
+	end
+
+	scenario "fail with blank password" do
+		sign_in_with user.email, ''
+		
+		expect(page).to have_text('Invalid email or password')
+	end
+
+	scenario "fail with wrong password" do
+		sign_in_with user.email, 'somewrongpassword'
+
+		expect(page).to have_text('Invalid email or password')
+	end
+
+	scenario "success with valid information" do
+		sign_in user
+
+		expect(page).to have_text('Signed in successfully')
+	end
+
+	def sign_in_with email, password
+		visit new_user_session_path
+		fill_in "Email", with: email
+		fill_in "Password", with: password
+		click_button "Sign in"
+	end
+
+	def sign_in user
+		sign_in_with user.email, user.password
+	end
+end
